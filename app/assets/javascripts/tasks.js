9133c3580697fdbe86e228758744d7fb3b1fd693
Brian Keim <westxbk@gmail.com>
2016-05-04 14:24:39 -0400

    First commit

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,13 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$(document).on("ready page:load", ready);
+
+function ready() {
+  $('#new_task').on('ajax:success', newTask);
+
+  function newTask(event, data) {
+   $('#tasks').append(data);
+   $('#task_name').val('');
+  }
+}
